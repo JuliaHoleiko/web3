06db624b9147531b3201f009dfd4f4c1f7afe526
JuliaHoleiko <juliagoleiko2341@gmail.com>
2022-11-03 10:22:07 +0200

    added view button on home page

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,20 +1,22 @@
-import React from 'react'
+import {React, useState} from 'react'
 import Heading from './Heading'
 import Card from '../components/Card'
 import styles from './Home.module.css'
 import Button from '../components/Button'
 export default function Home() {
+const [values, changeValue] = useState([1,2,3]);
+const preCatalog = values.map((item)=>
+<Card key={item} number ={item}/>)
+
+
   return (
     <div className={styles.home}>
       
         <Heading/>
         <div className={styles.cards}>
-
-        <Card number = '1'/>
-        <Card number = '1'/>
-        <Card number = '1'/>
+       {preCatalog}
         </div>
-        <Button className={styles.view_more} text = "View more"/>
+        <Button className={styles.view_more} text = "View more"  onClick = {()=> {changeValue([1,2,3,4,5,6])}}/>
         
 
     </div>
